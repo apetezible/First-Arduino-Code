a8cb60543676a65ef7a1d30f211b7a6757ba496b
Sebastian Sarmiento Mendoza <pituraapetezible@gmail.com>
2019-10-16 08:13:52 -0500

    Add files via upload

---
+++ Leds.ino
@@ -0,0 +1,15 @@
+int Led = 13;
+int Espera = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (Led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite (Led, HIGH);
+  delay (Espera * 2);
+  digitalWrite (Led, LOW);
+  delay (Espera);
+}
